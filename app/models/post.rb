f7bee8a2e97ef42f457abd14b4b0e6fb69c8a1dc
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-16 09:05:46 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+  #ActiveModel::Validator
+
+  validates :title, presence: true
+
+  validates :content, length: { minimum: 10 }
+
+  validates :summary, length: {maximum: 100}
+
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  validate :clickbate
+
+
+  def clickbate
+    words = [/Won't Believe/i, /Secret/i, /Top
+    [1-100]*/i, /Guess/i]
+    included = words.any? {|word| word.match title}
+    if !included
+      errors.add(:title, "needs to be clickbait-y")
+    end
+  end
 end
